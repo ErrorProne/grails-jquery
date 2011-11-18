282bd4a588c1dc6258a0a2e6846bfca54ebaa512
antonevane <antonevane@gmail.com>
2011-11-18 08:25:03 +0400

    Fix install script

--- scripts/InstallJQuery.groovy
+++ scripts/InstallJQuery.groovy
@@ -2,7 +2,7 @@ Ant.property(environment:"env")
 grailsHome = Ant.antProject.properties."env.GRAILS_HOME"
 
 // Hard coded for installation purpose
-def jQueryVersion = '1.6.1'
+def jQueryVersion = '1.7'
 def jQuerySources = 'jquery'
 
 includeTargets << grailsScript("_GrailsEvents")
