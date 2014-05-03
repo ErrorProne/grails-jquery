26d6821d29837dfa4801f3881dc6e7801f6adaa9
Bobby Warner <bobbywarner@gmail.com>
2014-05-03 09:16:10 -0500

    Updated jQuery to 1.11.1

--- src/groovy/org/codehaus/groovy/grails/plugins/jquery/JQueryConfig.groovy
+++ src/groovy/org/codehaus/groovy/grails/plugins/jquery/JQueryConfig.groovy
@@ -26,7 +26,7 @@ class JQueryConfig implements GrailsApplicationAware {
     def defaultPlugins
     def plugins = [:]
 
-    static SHIPPED_VERSION = '1.11.0'
+    static SHIPPED_VERSION = '1.11.1'
     GrailsApplication grailsApplication
 
     def init() {
